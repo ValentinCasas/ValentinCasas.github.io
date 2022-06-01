628918a487482a5814ff271fa3a201de3015e15f
ValentinCasas <101840863+ValentinCasas@users.noreply.github.com>
2022-06-01 17:54:20 -0300

    .

--- smog/jascript.js
+++ smog/jascript.js
@@ -1,36 +1,35 @@
 /*8888888888888888888888888888888888888888888 FORMULARIO 1 88888888888888888888888888888888888888888888888*/
-let countIntentos = 0;
 
-function valida_envia() {
-    
-    if (countIntentos == 0) {
+let intentos=1;
+
+function valida_envia(e) {
+    e.preventDefault();
+
     let name=document.fvalida.nombre.value;
-    let nameCss=document.getElementById('nombre');
-
     let edad = document.fvalida.edad.value;
-    let edadCss=document.getElementById('edad');
-
     let mensage = document.fvalida.mensage.value;
-    let mensageCss=document.getElementById('opinion');
-
     let mail = document.fvalida.direccionCorreo.value
-    let mailCss = document.getElementById('email');
-
+    
+    let errores=["Agregar un nombre","Error en el mail","La edad debe ser mayor o igual a 10","No se encontró el texto"];
+    let muestroErrores="";
+    const para = document.createElement("p");
 
     if (name.length == 0) {
-     
-        nameCss.style.backgroundColor = `#ff01018d`;
-        nameCss.style.color = `#000`;
+        document.fvalida.nombre.classList.add('invalid')
+        
+        muestroErrores = document.createTextNode(errores[0]);
+        para.appendChild(muestroErrores);
+        document.getElementById("mostrar").appendChild(para);
+
         document.fvalida.nombre.focus();
         return false;
     }else{
-        nameCss.style.backgroundColor = `#434343`
-        nameCss.style.color = `#726659`
+        document.fvalida.nombre.classList.remove('invalid')
+        
     }
-
+   
 
     //valido el mail
-
     let buscoarroba = false
     let buscopunto = false
     let posicionarroba = -1
@@ -46,52 +45,87 @@ function valida_envia() {
         }
     }
     if ((buscoarroba && buscopunto) && (posicionarroba < posicionpunto)) {
-        mailCss.style.backgroundColor = `#434343`
-        mailCss.style.color = `#726659`
+        document.fvalida.email.classList.remove('invalid')
     }
     else {
-        mailCss.style.backgroundColor = `#ff01018d`
-        mailCss.style.color = `#fff`
-        return false;
+        document.fvalida.email.classList.add('invalid')
 
+        muestroErrores = document.createTextNode(errores[1]);
+        para.appendChild(muestroErrores);
+        document.getElementById("mostrar").appendChild(para);
+        
+        return false;
     }
 
     //valido la edad
-
     if(edad < 10 || edad.length == 0){
-        edadCss.style.backgroundColor = `#ff01018d`
-        edadCss.style.color = `#fff`
+        document.fvalida.edad.classList.add('invalid')
+
+            muestroErrores = document.createTextNode(errores[2]);
+            para.appendChild(muestroErrores);
+            document.getElementById("mostrar").appendChild(para);
+         
         return false;
     }else{
-        edadCss.style.backgroundColor = `#434343`
-        edadCss.style.color = `#726659`
+        document.fvalida.edad.classList.remove('invalid')
     }
 
-    //valido textArea
 
+    
+    
+    //valido textArea
     if (mensage.length == 0) {
-        mensageCss.style.backgroundColor = `#ff01018d`
-        mensageCss.style.color = `#fff`
+        document.fvalida.mensage.classList.add('invalid')
+
+           muestroErrores = document.createTextNode(errores[3]);
+           para.appendChild(muestroErrores);
+            document.getElementById("mostrar").appendChild(para);
+         
    return false;
     }else{
-        mensageCss.style.backgroundColor = `#434343`
-        mensageCss.style.color = `#726659`
+        document.fvalida.mensage.classList.remove('invalid')
     }
 
-    if (name.length > 0 && mensage.length > 0 && mail.length > 0 && buscoarroba == true && buscopunto == true && edad >=10 && edad.length > 0 ) {
-        document.mostrar.innerHTML += "<p>" + "Nombre: " + name + "</p>";
-        document.mostrar.innerHTML += "<p>" + "E-mail: " + mail + "</p>";
-        document.mostrar.innerHTML += "<p>" + "Edad: " + edad + "</p>";
-        document.mostrar.innerHTML += "<p>" + "Opinion:</p>";
-        document.mostrar.innerHTML += "<p>" + mensage + "</p>";
-        document.fvalida.innerHTML += "<center><p>recibido</p>   <input type=checkbox checked> </center>";
-        countIntentos = countIntentos + 1;
-    } 
 
-}else{
-    document.fvalida.button.style.backgroundColor = `#ff01018d`,2000;
+
+    document.getElementById("mostrar").parentNode.removeChild(para);
+    // document.getElementById("mostrar").removeChild(mostrarErrores);
+   // document.getElementById("mostrar").removeChild(errores[i]);
+   
+        
+   // document.getElementById("mostrar").removeChild(errores);
+//AAAAAAAAAAAAAAAAAA
+    
+
+
+// Creo un nodo texto:
+const muestroNombre = document.createTextNode("Nombre: " + name+" - ");
+const muestroEdad = document.createTextNode("Edad: "+edad+" - ");
+const muestroMail = document.createTextNode("Email: "+mail+" - ");
+const muestroMensage = document.createTextNode("Mensaje: "+mensage);
+
+//le agrego a <p> el nodo texto
+para.appendChild(muestroNombre);
+document.getElementById("mostrar").appendChild(para);
+
+para.appendChild(muestroEdad);
+document.getElementById("mostrar").appendChild(para);
+
+para.appendChild(muestroMail);
+document.getElementById("mostrar").appendChild(para);
+
+para.appendChild(muestroMensage);
+document.getElementById("mostrar").appendChild(para);
+
+document.fvalida.innerHTML += "<p> mensajes recibidos: " + intentos + "</p>";
+
+ intentos++;
+
+
+
+
 }
-}
+
 
 /*8888888888888888888888888888888888888888888 GALERIA 888888888888888888888888888888888888888888888888*/
 
@@ -115,23 +149,16 @@ punto.forEach((cadaPunto, i) => {
 
         // MOVEMOS el grand
         grande.style.transform = `translateX(${operacion}%)`
-        if (posicion % 2 == 0) {
-            carrousel.style.background = `#282727`
-            
-        } else {
-            carrousel.style.background = `#3d3b3b`
-        }
-
 
         // Recorremos TODOS los punto
         punto.forEach((cadaPunto, i) => {
             // Quitamos la clase ACTIVO a TODOS los punto
             punto[i].classList.remove('activo')
-            punto[i].style.borderRadius = "4%"
+            
         })
         // Añadir la clase activo en el punto que se hizo CLICK
         punto[i].classList.add('activo')
-        punto[i].style.borderRadius = "100%"
+       
         
     })
   
@@ -172,15 +199,14 @@ punto2.forEach((cadaPunto, i) => {
         punto2.forEach((cadaPunto, i) => {
             // Quitamos la clase ACTIVO a TODOS los punto
             punto2[i].classList.remove('activo2')
-            punto2[i].style.borderRadius = "4%"
         })
         // Añadir la clase activo en el punto que se hizo CLICK
         punto2[i].classList.add('activo2')
-        punto2[i].style.borderRadius = "100%"
         
     })
   
 
 })
 
+/*8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888*/
 
